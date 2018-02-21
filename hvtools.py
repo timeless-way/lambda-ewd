047971b5d83fcf584e14d9cb3583fd39fe97aece
Huub van Thienen <huub@timeless-way.nl>
2018-02-21 14:16:42 +0100

    Tool refinement.

--- hvtools.py
+++ hvtools.py
@@ -4,10 +4,24 @@
 Created on Tue Feb 20 22:39:29 2018
 
 @author: huub
+
+Handige tools van HvT voor het werken met tatsu grammatica's
 """
+import tatsu
 
 def read_file(name):
     with open(name, 'r') as input:
         contents = input.read()
     return contents
 
+def write_file(name, contents):
+    with open(name, 'w') as output:
+        output.write(contents)
+
+def generate_parser(basename):
+    grammarname = basename + '.tatsu'
+    filename = basename + '.parser.py'
+    classname = basename[0].upper() + basename[1:]
+    grammar = read_file(grammarname)
+    p = tatsu.to_python_sourcecode(grammar, classname)
+    write_file(filename, p)
