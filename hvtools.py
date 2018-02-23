68706341b974043fec7adf6005f206687a466da1
Huub van Thienen <huub@timeless-way.nl>
2018-02-23 01:10:33 +0100

    Very first and very, very rudimentary version of the ewd grammar.

--- hvtools.py
+++ hvtools.py
@@ -18,10 +18,17 @@ def write_file(name, contents):
     with open(name, 'w') as output:
         output.write(contents)
 
-def generate_parser(basename):
+def generate_python_module(basename):
     grammarname = basename + '.tatsu'
-    filename = basename + '.parser.py'
+    filename = basename + '_parser.py'
     classname = basename[0].upper() + basename[1:]
     grammar = read_file(grammarname)
     p = tatsu.to_python_sourcecode(grammar, classname)
     write_file(filename, p)
+
+def generate_parser_instance(basename):
+    grammarname = basename + '.tatsu'
+    classname = basename[0].upper() + basename[1:]
+    grammar = read_file(grammarname)
+    parser = tatsu.compile(grammar, name=classname)
+    return parser
