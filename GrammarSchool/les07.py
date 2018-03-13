8e6310134ee2a383922f2730b9b4e10273c641c3
Huub van Thienen <huub@timeless-way.nl>
2018-03-13 13:40:48 +0100

    Packrat parsing with a more python-like datastructure.

--- GrammarSchool/les07.py
+++ GrammarSchool/les07.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar  8 18:08:00 2018
+Created on Tue Mar 13 12:34:29 2018
 
 @author: huub
 """
-full_backtrack = True
+
+derivs = []
 
 class Deriv:
     
-    def __init__(self, text):
+    def __init__(self, char):
         self.dvAdditive = None
         self.dvMultitive = None
         self.dvPrimary = None
         self.dvDecimal = None
-        if len(text) > 0:
-            self.dvChar = Result(text[0], Deriv(text[1:]))
-        else:
-            self.dvChar = Result('', None)
+        self.char = char
+    
+    def succ(self):
+        ident = derivs.index(self)
+        return derivs[ident+1]
     
 class Result:
     
     def __init__(self, result, next_deriv):
         self.result = result
-        self.next_deriv = next_deriv
+        self.continuation_deriv = next_deriv
 
 def pAdditive(deriv):
     if deriv.dvAdditive:
@@ -31,17 +33,15 @@ def pAdditive(deriv):
     # First alternative of the grammar
     multitive = pMultitive(deriv)
     if multitive:
-        rest = multitive.next_deriv
-        if check_initial_char(rest, '+'):
-            res = pAdditive(rest.dvChar.next_deriv)
-            if res:
-                n = multitive.result + res.result
-                result = Result(n, res.next_deriv)
+        continue_deriv = multitive.continuation_deriv
+        if check_initial_char(continue_deriv, '+'):
+            additive = pAdditive(continue_deriv.succ())
+            if additive:
+                n = multitive.result + additive.result
+                result = Result(n, additive.continuation_deriv)
                 deriv.dvAdditive = result
                 return result
     # Second alternative
-    if full_backtrack:
-        multitive = pMultitive(deriv) # Do this again for backtracking!
     deriv.dvAdditive = multitive
     return multitive
 
@@ -51,17 +51,15 @@ def pMultitive(deriv):
     # First alternative of the grammar
     primary = pPrimary(deriv)
     if primary:
-        rest = primary.next_deriv
-        if check_initial_char(rest, '*'):
-            res = pMultitive(rest.dvChar.next_deriv)
-            if res:
-                n = primary.result * res.result
-                result = Result(n, res.next_deriv)
+        continue_deriv = primary.continuation_deriv
+        if check_initial_char(continue_deriv, '*'):
+            multitive = pMultitive(continue_deriv.succ())
+            if multitive:
+                n = primary.result * multitive.result
+                result = Result(n, multitive.continuation_deriv)
                 deriv.dvMultitive = result
                 return result
     # Second alternative
-    if full_backtrack:
-        primary = pPrimary(deriv) # Do this again for backtracking!
     deriv.dvMultitive = primary
     return primary
 
@@ -70,10 +68,10 @@ def pPrimary(deriv):
         return deriv.dvPrimary
     # First alternative of the grammar
     if check_initial_char(deriv, '('):
-        additive = pAdditive(deriv.dvChar.next_deriv)
+        additive = pAdditive(deriv.succ())
         if additive:
-            if check_initial_char(additive.next_deriv, ')'):
-                result = Result(additive.result, additive.next_deriv.dvChar.next_deriv)
+            if check_initial_char(additive.continuation_deriv, ')'):
+                result = Result(additive.result, additive.continuation_deriv.succ())
                 deriv.dvPrimary = result
                 return result
     # Second alternative
@@ -84,11 +82,11 @@ def pPrimary(deriv):
 def pDecimal(deriv):
     if deriv.dvDecimal:
         return deriv.pDecimal
-    if deriv.dvChar.result == '':
+    if deriv.char == '':
         #print('Expected 0..9 but found nothing')
         return None
-    elif deriv.dvChar.result in ['0','1','2','3','4','5','6','7','8','9']:
-        result = Result(int(deriv.dvChar.result), deriv.dvChar.next_deriv)
+    elif deriv.char in ['0','1','2','3','4','5','6','7','8','9']:
+        result = Result(int(deriv.char), deriv.succ())
         deriv.dvDecimal = result
         return result
     else:
@@ -96,14 +94,20 @@ def pDecimal(deriv):
         return None
 
 def check_initial_char(deriv, char):
-    return deriv.dvChar != None and deriv.dvChar.result == char
+    return deriv.char != None and deriv.char == char
 
 def parse(text):
-    derivs = Deriv(text)
-    pAdditive(derivs)
+    global derivs
+    derivs = [0] * (len(text) + 1)
+    i = 0
+    for char in text:
+        derivs[i] = Deriv(char)
+        i += 1
+    derivs[len(text)] = Deriv(None)
+    pAdditive(derivs[0])
     return derivs
 
-text = '(5+4*3)*(3+4)+(3*4*5)'
+text = '(5+4*3)*9'
 derivs = parse(text)
-print(text, '=', derivs.dvAdditive.result)
+print(text, '=', derivs[0].dvAdditive.result)
 
