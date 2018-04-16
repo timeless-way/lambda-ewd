c0375a80d52a8901fddb505afd979bcfb82f8462
Huub van Thienen <huub@timeless-way.nl>
2018-04-16 11:52:43 +0200

    Extended functionality of codegenerator.

--- codegenerator.py
+++ codegenerator.py
@@ -9,6 +9,15 @@ import collections
 
 class CodeGenerator:
     
+    def c_program(self, ast):
+        return self.remove_interpunction(ast, [";"])
+    
+    def c_super_combinator(self, ast):
+        return ESc(lhs=ast['lhs'], expr=ast['expr'])
+    
+    def c_lhs(self, ast):
+        return ELhs(name=ast['name'], pars=ast['pars'])
+    
     def c_number(self, ast):
         return ENum(int(ast))
     
@@ -20,6 +29,12 @@ class CodeGenerator:
     
     def c_letrec_expression(self, ast):
         return ELet(isRec=True, deflist=ast['deflist'], expr=ast['expr'])
+
+    def c_definition_list(self, ast):
+        return self.remove_interpunction(ast, [";"])
+    
+    def c_definition(self, ast):
+        return EBind(name=ast['name'], val=ast['val'])
     
     def c_case_expression(self, ast):
         return ECase(expr=ast['expr'], alts=ast['alts'])
@@ -28,7 +43,7 @@ class CodeGenerator:
         return EAlt(tag=ast['tag'], vars=ast['vars'], expr=ast['expr'])
     
     def c_lambda_expression(self, ast):
-        return ELam(pars=ast['pars'], body=ast['body'])
+        return self.create_lambda_abstraction(ast['pars'], ast['body'])
     
     def c_constructor(self, ast):
         return EConstr(tag=ast['tag'], arity=ast['arity'])
@@ -96,13 +111,48 @@ class CodeGenerator:
             ap = EAp(fun=ap, arg=arg)
             i = i+1
         return ap
+    
+    def create_lambda_abstraction(self, pars, body):
+        if len(pars) == 1:
+            return ELam(par=pars[0], body=body)
+        else:
+            abstr = self.create_lambda_abstraction(pars[1:], body)
+            return ELam(par=pars[0], body=abstr)
+    
+    def remove_interpunction(self, sequence, interpunctions):
+        return [item for item in sequence if item not in interpunctions]
         
-
+ESc     = collections.namedtuple('ESc', 'lhs expr')
+ELhs    = collections.namedtuple('ELhs', 'name pars')
+EBind   = collections.namedtuple('EBind', 'name val')
 EVar    = collections.namedtuple('EVar', 'ident')
 ENum    = collections.namedtuple('ENum', 'intVal')
 EConstr = collections.namedtuple('EConstr', 'tag arity')
 EAp     = collections.namedtuple('EAp', 'fun arg')
 ELet    = collections.namedtuple('ELet', 'isRec deflist expr')
 ECase   = collections.namedtuple('ECase', 'expr alts')
-ELam    = collections.namedtuple('ELam', 'pars body')
+ELam    = collections.namedtuple('ELam', 'par body')
 EAlt    = collections.namedtuple('EAlt', 'tag vars expr')
+
+# The next section contains some utilities to manipulate the parse tree
+
+def is_atomic(expr):
+    return (isinstance(expr, EVar) or isinstance(expr, ENum))
+
+def left_hand_sides(program):
+    return [sc.lhs for sc in program.ast]
+
+def expressions(program):
+    return [sc.expr for sc in program.ast]
+
+def names(program):
+    return [lhs.name for lhs in left_hand_sides(program)]
+
+def parameters(program):
+    return [lhs.pars for lhs in left_hand_sides(program)]
+
+def binders_of(deflist):
+    return [dfn.name for dfn in deflist]
+
+def values_of(deflist):
+    return [dfn.val for dfn in deflist]
