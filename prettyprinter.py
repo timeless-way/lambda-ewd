e0b5d5daf498d5ab9ce56dc35fc962edcca41d75
Huub van Thienen <huub@timeless-way.nl>
2019-02-05 15:25:54 +0100

    flatten van printstructuren object georineteerd gemaakt.

--- prettyprinter.py
+++ prettyprinter.py
@@ -6,14 +6,60 @@ Created on Mon Apr  9 17:16:28 2018
 @author: huub
 """
 import collections
-import pprint
 
-INil = collections.namedtuple('INil', '')
-IStr = collections.namedtuple('IStr', 'contents')
-IWord = collections.namedtuple('IWord', 'contents')
-INewline = collections.namedtuple('INewline', '')
-IAppend = collections.namedtuple('IAppend', 'left right')
+_INil = collections.namedtuple('_INil', '')
+_IStr = collections.namedtuple('_IStr', 'contents')
+_IWord = collections.namedtuple('_IWord', 'contents')
+_INewline = collections.namedtuple('_INewline', '')
+_IAppend = collections.namedtuple('_IAppend', 'left right')
 
+class INil(_INil):
+  
+  def flatten(self, worklist, context):
+    flatten_tail(worklist, context)
+
+class IStr(_IStr):
+  
+  def flatten(self, worklist, context):
+    head = self.contents
+    tail = flatten_tail(worklist, context)
+    return head + tail
+
+class IWord(_IWord):
+
+  def flatten(self, worklist, context):
+    new_context = PrintContext(True, context.indent)
+    head = self.contents
+    tail = flatten_tail(worklist, new_context)
+    if context.separator:
+      s = ' '
+    else:
+      s = ''
+    return head + s + tail
+  
+class INewline(_INewline):
+    
+  def flatten(self, worklist, context):
+    tail = flatten_tail(worklist, context)
+    margin = '\n{:>{width}}'.format('', width=context.indent)
+    return margin + tail
+
+class IAppend(_IAppend):
+  
+  def flatten(self, worklist, context):
+    return self.left.flatten([self.right] + worklist, context)
+
+class PrintContext():
+  
+  def __init__(self, separator, indent):
+    self.separator = separator
+    self.indent = indent
+
+def flatten_tail(worklist, context):
+  if len(worklist) > 0:
+    return worklist[0].flatten(worklist[1:], context)
+  return ''
+  
 def append(*contents):
   contents = list(filter(lambda c: not isinstance(c, INil), contents))
   if len(contents) == 0:
@@ -25,41 +71,8 @@ def append(*contents):
     for c in contents[2:]:
       tree = IAppend(tree, c)
   return tree
-
-def flatten(iseq_list, separator, indent):
-  if (len(iseq_list) == 0):
-    return ''
-  iseq = iseq_list[0]
-  iseq_list_tail = iseq_list[1:]
-  if isinstance(iseq, INil):
-    return flatten(iseq_list_tail, separator, indent)
-  elif isinstance(iseq, IStr):
-    return iseq.contents + flatten(iseq_list_tail, False, indent)
-  elif isinstance(iseq, IWord):
-    return flatten_word(iseq_list, separator, indent)
-  elif isinstance(iseq, INewline):
-    return flatten_newline(iseq_list, separator, indent)
-  elif isinstance(iseq, IAppend):
-    return flatten([iseq.left] + [iseq.right] + iseq_list_tail, separator, indent)
-  else:
-    print('Unknown type for iseq ' + iseq)
-    return flatten(iseq_list_tail, separator, indent)
-
-def flatten_word(iseq_list, separator, indent):
-  iseq = iseq_list[0]
-  iseq_list_tail = iseq_list[1:]
-  s1 = flatten(iseq_list_tail, True, indent)
-  if separator:
-    s0 = ' '
-  else:
-    s0 = ''
-  return s0 + iseq.contents + s1
-
-def flatten_newline(iseq_list, separator, indent):
-  iseq_list_tail = iseq_list[1:]
-  margin = '\n{:>{width}}'.format('', width=indent)
-  return margin + flatten(iseq_list_tail, False, indent)
   
 def display(expr):
+  context = PrintContext(False, 0)
   print_struct = expr.pp()
-  return flatten([print_struct], False, 0)
+  return print_struct.flatten([], context)
